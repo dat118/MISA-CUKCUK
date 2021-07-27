bfa0196e61517062c0a85f9ee4ea6107b0a2f19e
dat118 <dat1182000@gmail.com>
2021-07-27 08:03:26 +0700

    27/7

--- js/base.js
+++ js/base.js
@@ -1,34 +1,56 @@
-// class BaseJS{
+// class Base{
 //     constructor(){
 //         this.loadData();
+        
 //     }
 
 //     loadData(){
-//         // Lấy thông tin các cột
-//         // var fields = [{index: '1', name : 'EmployeeCode'},{ index:'2', name:'FullName'},{ index:'3',name:'Gender'}];
+        
          
-//         var ths = $('table thead th'); 
-//         var fieldNames = [];
-//         // Lay du lieu de MAP
+//         //Gọi api lấy dữ liệu
         
 //         $.ajax({
+//             type: "GET",
 //             url: "http://cukcuk.manhnv.net/v1/Employees",
-//             method: 'GET',
             
-//         }).done(function(res){ 
-//             alert('sadsa')
-//             // $.each(res,function(index,obj){
-//             //     var tr = $(`<tr></tr>`);
-//             //     $.each( ths, function(index , th) {
-//             //         var td = `<td><div><span>sdasaddsa</span></div></td>`
-//             //         var fieldName = $(th).attr('fieldName');
-//             //         $(tr).append(td);
-//             //        }) 
-//             //        $('table tbody').append(tr);
-//                 })
-
-        
-
-        
-//     })
+//             success: function (response) {
+//             // Xử lí dữ liệu
+            
+//             var tableEmployeeData = res;
+//             // console.log(res)
+//             $.each(tableEmployeeData, function (index, item) {
+//                 var dateOfBirth = item.DateOfBirth;
+//                 var money = item.Salary;
+//                 birthDay = formatDate(dateOfBirth);
+//                 money = formatMoney(money);
+                
+//                 item.EmployeeCode = item.EmployeeCode === null ? " " : item.EmployeeCode;
+//                 item.FullName = item.FullName === null ? " " : item.FullName;
+//                 item.GenderName = item.GenderName === null ? " " : item.GenderName;
+//                 item.PhoneNumber = item.PhoneNumber === null ? " " : item.PhoneNumber;
+//                 item.Email = item.Email === null ? " " : item.Email;
+//                 item.PositionName = item.PositionName === null ? " " : item.PositionName;
+//                 item.DepartmentName = item.DepartmentName === null ? " " : item.DepartmentName;
+//                 item.WorkStatus = item.WorkStatus === null ? " " : item.WorkStatus;
+//                 var tr = $(`<tr employeeid = '${item.EmployeeId}' >
+//             <td> <input type="checkbox"> </td>
+//             <td>`+ item.EmployeeCode + `</td>
+//             <td>`+ item.FullName + `</td>
+//             <td class="text-align-center" >`+ item.GenderName + `</td>
+//             <td class="text-align-center">`+ birthDay + `</td>
+//             <td class="text-align-center">`+ item.PhoneNumber + `</td>
+//             <td>`+ item.Email + `</td>
+//             <td>`+ item.PositionName + `</td>
+//             <td>`+ item.DepartmentName + `</td>
+//             <td class="text-align-right">`+ money + `</td>
+//             <td>`+ item.WorkStatus + `</td>
+//         </tr>` );
+                
+//                 $('table tbody').append(tr);
+            
+//             }
+            
+//         });
+       
+//     });
 // }
--- js/employee.js
+++ js/employee.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
+    formMode = null;
+    var employeeId = '';
+    loadData();
+});
+/**
+ * Hàm load dữ liệu
+ * Creator:NDD-19/7/2021
+ */
 
+function loadData(){
+    // Clean dữ liệu cũ
+    $('table tbody').empty();
     var tableEmployee = $('tbody');
     /**
      * Lấy dữ liệu về table
@@ -13,23 +24,25 @@ $(document).ready(function () {
         }).done(function (res) {
 
             var tableEmployeeData = res;
-            console.log(res)
+            // console.log(res)
             $.each(tableEmployeeData, function (index, item) {
                 var dateOfBirth = item.DateOfBirth;
                 var money = item.Salary;
                 birthDay = formatDate(dateOfBirth);
                 money = formatMoney(money);
-                item.EmployeeCode = item.EmployeeCode === null ? " " : item.EmployeeCode;
-                item.FullName = item.FullName === null ? " " : item.FullName;
-                item.GenderName = item.GenderName === null ? " " : item.GenderName;
-                item.PhoneNumber = item.PhoneNumber === null ? " " : item.PhoneNumber;
-                item.Email = item.Email === null ? " " : item.Email;
-                item.PositionName = item.PositionName === null ? " " : item.PositionName;
-                item.DepartmentName = item.DepartmentName === null ? " " : item.DepartmentName;
-                item.WorkStatus = item.WorkStatus === null ? " " : item.WorkStatus;
-                var tr = $(`<tr>
+                
+                item.EmployeeCode = item.EmployeeCode == null ? " " : item.EmployeeCode;
+                item.FullName = item.FullName == null ? " " : item.FullName;
+                item.GenderName = item.GenderName == null ? " " : item.GenderName;
+                item.PhoneNumber = item.PhoneNumber == null ? " " : item.PhoneNumber;
+                item.Email = item.Email == null ? " " : item.Email;
+                item.PositionName = item.PositionName == null ? " " : item.PositionName;
+                item.DepartmentName = item.DepartmentName == null ? " " : item.DepartmentName;
+                item.WorkStatus = item.WorkStatus == null ? " " : item.WorkStatus;
+                var tr = $(`<tr employeeid = '${item.EmployeeId}' >
+            <td class = "td-checked"> <input class="checked" type="checkbox" value = "${item.EmployeeId}"> </td>
             <td>`+ item.EmployeeCode + `</td>
-            <td>`+ item.FullName + `'</td>
+            <td>`+ item.FullName + `</td>
             <td class="text-align-center" >`+ item.GenderName + `</td>
             <td class="text-align-center">`+ birthDay + `</td>
             <td class="text-align-center">`+ item.PhoneNumber + `</td>
@@ -39,6 +52,7 @@ $(document).ready(function () {
             <td class="text-align-right">`+ money + `</td>
             <td>`+ item.WorkStatus + `</td>
         </tr>` );
+                
                 $('table tbody').append(tr);
             })
         }).fail(function () {
@@ -47,8 +61,8 @@ $(document).ready(function () {
     } catch (e) {
         console.log(e);
     }
-});
 
+}
 
 /**
  * Định dạng ngày sinh
@@ -90,7 +104,21 @@ function formatMoney(money) {
  */
 // Hiển thị Form
 $('#btn-add').click(function () {
+    formMode = 1;
+    // Hiển thị form thêm mới
     $('.profile-wrapper').show();
+    // Reset form
+    $('.profile-wrapper input').val(null);
+    // Lấy mã nhân viên tự động
+    $.ajax({
+        url:'http://cukcuk.manhnv.net/v1/Employees/NewEmployeeCode',
+        method:'GET'
+    }).done(res =>{
+        $('#code').val(res);
+        $('#code').focus();
+    }).fail(function(){
+        alert("Không lấy được mã nhân viên, vui lòng kiểm tra lại kết nối");
+    })
 })
 // Ẩn Form
 $('.button-cancel').click(function () {
@@ -117,74 +145,161 @@ $('.require').blur(function () {
         $(this).removeAttr('title');
     }
 })
-// Thêm thông tin
-$('#btn-save').click(function () {
-    let employee ={};
-    // Validate data
-    employee.FullName = $('#name').val();
-    employee.EmployeeCode = $('#code').val();
-    employee.PhoneNumber = $('#number').val();
-    employee.GenderName = $('#sex').val();
-    employee.Email = $('#mail').val();
-    employee.PositionName = $('#position').val();
-    employee.DepartmentName = $('#department').val();
-    employee.WorkStatus = $('#work-stt').val();
-    employee.DateOfBirth = $('#date').val();
-    employee.DateOfBirth = new Date(employee.DateOfBirth);
-    employee.IdentityNumber = $('#id').val();
-    employee.IdentityDate = $('#date-re').val();
-    employee.IdentityDate = new Date(employee.IdentityDate);
-    employee.IdentityPlace = $('#id-place').val();
-    employee.PersonalTaxCode = $('#tax').val();
-    employee.Salary = $('#salary').val();
-    employee.JoinDate = $('#join-date').val();
-    employee.JoinDate = new Date(employee.JoinDate);   
-    console.log(JSON.stringify(employee));
-    // Gọi đến API để lưu dữ liệu
 
-    // let employee = {
-    //     "createdDate": "2021-07-21T16:27:54.509Z",
-    //     "createdBy": "string",
-    //     "employeeId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-    //     "employeeCode": "NVf123444",
-    //     "fullName": "string",
-    //     "gender": 0,
-    //     "dateOfBirth": "2021-07-21T16:27:54.509Z",
-    //     "phoneNumber": "string",
-    //     "email": "string@strudfs.cjsha",
-    //     "identityNumber": "string",
-    //     "identityDate": "2021-07-21T16:27:54.509Z",
-    //     "identityPlace": "string",
-    //     "joinDate": "2021-07-21T16:27:54.509Z",
-    //     "departmentId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-    //     "positionId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-    //     "workStatus": 0,
-    //     "personalTaxCode": "string",
-    //     "salary": 0
-    // };
 
-    var settings = {
-        "url": `http://cukcuk.manhnv.net/v1/Employees/`,
-        "method": 'POST',
-        "timeout": 0,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "data": JSON.stringify(employee),
-    };
+/**
+ * Chỉnh sửa thông tin nhân viên
+ * Creator:NDDAT-22/7/2021
+ */
 
-    $.ajax(settings).done(res => {
-        console.log(res);
-        alert('Them thanh cong');
+// Chọn nhân viên chỉnh sửa
 
-    }).fail(res => {
-        alert('Đã có lỗi, vui lòng liên hệ MISA')
+    $('table#table-employee').on('dblclick','tbody tr',function(){
+        formMode = 0;
+        $('.profile-wrapper').show();
+    
+    // Hiển thị chi tiết
+     employeeId = $(this).attr('employeeid');
+    
+    $.ajax({
+        url:'http://cukcuk.manhnv.net/v1/Employees/'+employeeId,
+        method:'GET'
+    }).done(res =>{
+        var employee = res;
+        $('#name').val(employee.FullName);
+        $('#code').val(employee.EmployeeCode);
+        $('#number').val(employee.PhoneNumber);
+        $('#sex').val(employee.GenderName);
+        $('#mail').val(employee.Email);
+        $('#position').val(employee.PositionName);
+        $('#department').val(employee.DepartmentName);
+        $('#work-stt').val(employee.WorkStatus);
+        $('#date').val(formatDate(employee.DateOfBirth));
+        $('#id').val(employee.IdentityNumber);
+        $('#date-re').val(formatDate(employee.IdentityDate));
+        $('#id-place').val(employee.IdentityPlace);
+        $('#tax').val(employee.PersonalTaxCode);
+        $('#salary').val(formatMoney(employee.Salary));
+        $('#join-date').val(formatDate(employee.JoinDate));
+        console.log(employee);
+        console.log(employeeId);
+    }).fail (res => {
+        alert("Không thể lấy thông tin")
     })
-})
+});
 
+        //Chỉnh sửa thông tin
+        $('#btn-save').click(function () {
+            let employee ={};
+            // Validate data
+            
+            employee.FullName = $('#name').val();
+            employee.EmployeeCode = $('#code').val();
+            employee.PhoneNumber = $('#number').val();
+            employee.GenderName = $('#sex').val();
+            employee.Email = $('#mail').val();
+            employee.PositionName = $('#position').val();
+            employee.DepartmentName = $('#department').val();
+            employee.WorkStatus = $('#work-stt').val();
+            employee.DateOfBirth = $('#date').val();
+            // employee.DateOfBirth = new Date(employee.DateOfBirth);
+            employee.IdentityNumber = $('#id').val();
+            employee.IdentityDate = $('#date-re').val();
+            // employee.IdentityDate = new Date(employee.IdentityDate);
+            employee.IdentityPlace = $('#id-place').val();
+            employee.PersonalTaxCode = $('#tax').val();
+            employee.Salary = $('#salary').val().replaceAll('.','');
+            employee.Salary = employee.Salary == " " ? null : employee.Salary;
+            employee.JoinDate = $('#join-date').val();
+            // employee.JoinDate = new Date(employee.JoinDate);   
+            console.log(employee);
+            if (formMode == 1) {
+                var settings = {
+        
+                    "url" : `http://cukcuk.manhnv.net/v1/Employees/`,
+                    "method" : 'POST',  
+                    "timeout": 0,
+                    "headers": {
+                        "Content-Type": "application/json"
+                    },
+                    "data": JSON.stringify(employee),
+                };
+            
+                $.ajax(settings).done(res => {
+                    console.log(res);
+                    alert('Thêm thành công');
+                    loadData();
+                    $('.profile-wrapper').hide();
+                }).fail(res => {
+                    alert('Đã có lỗi, vui lòng liên hệ MISA')
+                })
+                
+            } else {
+                employee.EmployeeId = employeeId; 
+                // Gọi đến API để lưu dữ liệu
+            // console.log(employeeId);
+            var settings = {
+            
+                "url" : `http://cukcuk.manhnv.net/v1/Employees/`+employeeId,
+                "method" : 'PUT',  
+                "timeout": 0,
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "data": JSON.stringify(employee),
+            };
+        
+            $.ajax(settings).done(res => {
+                console.log(res);
+                alert('Thay đổi thành công');
+                loadData();
+                $('.profile-wrapper').hide();
+            }).fail(res => {
+                alert('Đã có lỗi, vui lòng liên hệ MISA')
+            })  
+            }  
+    });
 
+    /**
+     * Xóa nhân viên
+     * Creator:NDDAT-25-7-2021
+     */
 
+    $('#btn-delete').click(function(){
 
+        console.log(employeeId);
+        var settings = {
+            
+            "url" : `http://cukcuk.manhnv.net/v1/Employees/`+employeeId,
+            "method" : 'DELETE',  
+        };
+    
+        $.ajax(settings).done(res => {
+            console.log(res);
+            alert('Xóa thành công');
+            loadData();
+            $('.profile-wrapper').hide();
+        }).fail(res => {
+            alert('Xóa không thành công')
+        })  
+    })
+
+//    var check = [];
+//    check = $('.checked:checkbox:checked').parent('td-check').parent().css( "background", "yellow" );
+//    console.log(check);
+// var arr = [];
+// $('.checked:checkbox:checked').each(function () {
+//     arr.push(this.value);
+// });
+// console.log(arr);
+// let myArray = (function() {
+//     let a = [];
+//     $(".checked:checked").each(function() {
+//         a.push(this.value);
+//     });
+//     return a;
+// })
+// console.log(myArray);
 
 // /**
 //  * Validate email
